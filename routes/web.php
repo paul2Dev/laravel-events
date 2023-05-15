6d2e3d548ffef9c0ae6cf23e33fddbeb01316236
Paul2D <paul.maxineanu@gmail.com>
2023-05-15 17:37:05 +0300

    install and configure laravel-google-calendar package

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/google-calendar', function () {
+    $events = \Spatie\GoogleCalendar\Event::get()->first();
+    dd($events->name);
+});
